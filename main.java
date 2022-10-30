bd3c7cdbb449d240b710959e75dbb3ab94bfa37d
splach-coder <76971107+splach-coder@users.noreply.github.com>
2022-10-30 13:15:16 +0100

    adding the game

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("hello world from vc");
-        System.out.println("some changes");
+        System.out.println("Hangman Game with Java Keywords :)");
+        HangmanGame hangmanGame = new HangmanGame();
+        hangmanGame.newGame();
+        hangmanGame.play();   
     }
 }
